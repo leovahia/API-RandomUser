f927c29dfea102a6a7fee84e0d83cc3501c80568
Leonardo Saboia <leovahia@gmail.com>
2021-03-26 23:42:17 -0300

    versão aperfeiçoada

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const faker = require('faker');
+const moment = require('moment');
 
 faker.locale = 'pt_BR';
 
 const people = [];
 
     for (let i = 0; i < 10; i++) {
+
         const person = {
+
             firstName: faker.name.firstName(),
             lastName: faker.name.lastName(),
             gender: faker.name.gender(),
-            birthDay: faker.date.past(),
+            birthDay: moment(faker.date.past('01','2003')).format('DD/MM/YYYY'),
             email: faker.internet.email(),
             userName: faker.internet.userName(),
             password: faker.internet.password(),
