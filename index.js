8e7cb6cfbf01dee13e40012c109ddcfab8c4a256
Leonardo Saboia <63628556+leovahia@users.noreply.github.com>
2021-03-28 18:58:34 -0300

    complete project

---
+++ index.js
@@ -0,0 +1,13 @@
+const people = require('./main');
+
+const express = require('express')
+const api = express();
+const port = 3000;
+
+api.get('/', (req, res)=>{
+    res.send(people);
+});
+
+api.listen(port, ()=>{
+    console.log(`API está ativa no http://localhost:${port}`);
+});
